80f51f1e2a44ef804aa301dea6c5b84df5ff3f45
Jonariar Deva Wardhani <47747520+devaward@users.noreply.github.com>
2022-10-15 22:41:01 +0700

    Create bookmark.js

---
+++ animein/v4.1/assets/js/bookmark.js
@@ -0,0 +1,60 @@
+function setCookie(cname, cvalue, exdays) {
+	const d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	let expires = "expires="+ d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+	for(let i = 0; i <ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+window.addEventListener('load', () => {
+	const bookmarkBtn = document.querySelector('.post_btnReact .btn.bookmark');
+	const postEl = document.querySelector('.jona_animeinfo .item-post');
+	
+	if(bookmarkBtn && postEl) {
+		let postId = postEl.getAttribute('post-id');
+		let bookmarks = getCookie('bookmarks');
+		let currentCookie = !bookmarks ? [] : JSON.parse(bookmarks);
+		let hasId = currentCookie.indexOf(postId);
+		
+		/* [ Check Cookies ] */
+		let title = bookmarkBtn.querySelector('span');
+		if(hasId === -1) {
+			bookmarkBtn.classList.add('enabled');
+			title.textContent = 'Bookmark';
+		} else {
+			bookmarkBtn.classList.remove('enabled');
+			title.textContent = 'Bookmarked';
+		}
+		
+		/* [ Btn Cookies ] */
+		bookmarkBtn.onclick = () => {
+			postId = postEl.getAttribute('post-id');
+			bookmarks = getCookie('bookmarks');
+			currentCookie = !bookmarks ? [] : JSON.parse(bookmarks);
+			hasId = currentCookie.indexOf(postId);
+			if(hasId === -1) {
+				currentCookie.push(postId);
+				bookmarkBtn.classList.add('enabled');
+				title.textContent = 'Bookmarked';
+			} else {
+				currentCookie.splice(hasId, 1);
+				bookmarkBtn.classList.remove('enabled');
+				title.textContent = 'Bookmark';
+			}
+			setCookie('bookmarks', JSON.stringify(currentCookie), 360);
+		}
+	}
+});
