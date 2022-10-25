e8f1e04e1be075ba3a571b015935aac300c78db7
Jonariar Deva Wardhani <47747520+devaward@users.noreply.github.com>
2022-10-25 10:05:46 +0700

    Update main.js

--- animein/v4.1/assets/js/main.js
+++ animein/v4.1/assets/js/main.js
@@ -239,8 +239,8 @@ try {
 
   /* [Episode List] */
   let jonanime_id = document.querySelector('.jona_animepost').getAttribute('jonanime_id');
-  let geteps = `/feeds/posts/default?alt=json-in-script&callback=episodeList_byId&start-index=1&max-results=9999&q=label:${encodeURIComponent(jonanime_id)}`;
-  let episodeList_byId = ({feed}) => {
+  let geteps = `/feeds/posts/default?alt=json-in-script&callback=episodeListbyId&start-index=1&max-results=9999&q=label:${encodeURIComponent(jonanime_id)}`;
+  let episodeListbyId = ({feed}) => {
   	const {entry} = feed;
   	for(e of entry) {
   		console.log(e);
