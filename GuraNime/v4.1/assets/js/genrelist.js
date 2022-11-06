0958ceb051fb5f71c3f75eb687a9596789cea5d6
Jonariar Deva Wardhani <47747520+devaward@users.noreply.github.com>
2022-11-06 22:53:32 +0700

    Create genrelist.js

---
+++ GuraNime/v4.1/assets/js/genrelist.js
@@ -0,0 +1,252 @@
+let genrelist = {
+  "anime" : [
+    {
+      "type": "Genres",
+      "list": [
+      	{
+      		"title": "Action",
+      		"label" : "Action"
+      	},{
+      		"title": "Adventure",
+      		"label" : "Adventure"
+      	},{
+      		"title": "Avant Garde",
+      		"label" : "Avant Garde"
+      	},{
+      		"title": "Award Winning",
+      		"label" : "Award Winning"
+      	},{
+      		"title": "Boys Love",
+      		"label" : "Boys Love"
+      	},{
+      		"title": "Comedy",
+      		"label" : "Comedy"
+      	},{
+      		"title": "Drama",
+      		"label" : "Drama"
+      	},{
+      		"title": "Fantasy",
+      		"label" : "Fantasy"
+      	},{
+      		"title": "Girls Love",
+      		"label" : "Girls Love"
+      	},{
+      		"title": "Gourmet",
+      		"label" : "Gourmet"
+      	},{
+      		"title": "Horror",
+      		"label" : "Horror"
+      	},{
+      		"title": "Mystery",
+      		"label" : "Mystery"
+      	},{
+      		"title": "Romance",
+      		"label" : "Romance"
+      	},{
+      		"title": "Sci-Fi",
+      		"label" : "Sci-Fi"
+      	},{
+      		"title": "Slice of Life",
+      		"label" : "Slice of Life"
+      	},{
+      		"title": "Sports",
+      		"label" : "Sports"
+      	},{
+      		"title": "Supernatural",
+      		"label" : "Supernatural"
+      	},{
+      		"title": "Suspense",
+      		"label" : "Suspense"
+      	}
+      ]
+    },{
+    	"type": "Explicit Genres",
+    	"list": [
+    		{
+    			"title": "Ecchi",
+    			"label": "Ecchi"
+    		},{
+    			"title": "Erotica",
+    			"label": "Erotica"
+    		},{
+    			"title": "Hentai",
+    			"label": "Hentai"
+    		}
+    	]
+    },{
+    	"type": "Themes",
+    	"list": [
+    		{
+    			"title": "Adult Cast",
+    			"label": "Adult Cast"
+    		},{
+    			"title": "Anthromorphic",
+    			"label": "Anthromorphic"
+    		},{
+    			"title": "CGDCT",
+    			"label": "CGDCT"
+    		},{
+    			"title": "Childcare",
+    			"label": "Childcare"
+    		},{
+    			"title": "Combat Sports",
+    			"label": "Combat Sports"
+    		},{
+    			"title": "Crossdressing",
+    			"label": "Crossdressing"
+    		},{
+    			"title": "Delinquents",
+    			"label": "Delinquents"
+    		},{
+    			"title": "Detective",
+    			"label": "Detective"
+    		},{
+    			"title": "Educational",
+    			"label": "Educational"
+    		},{
+    			"title": "Gag Humor",
+    			"label": "Gag Humor"
+    		},{
+    			"title": "Gore",
+    			"label": "Gore"
+    		},{
+    			"title": "Harem",
+    			"label": "Harem"
+    		},{
+    			"title": "Historical",
+    			"label": "Historical"
+    		},{
+    			"title": "Idols (Female)",
+    			"label": "Idols (Female)"
+    		},{
+    			"title": "Idols (Male)",
+    			"label": "Idols (Male)"
+    		},{
+    			"title": "Isekai",
+    			"label": "Isekai"
+    		},{
+    			"title": "Iyashikei",
+    			"label": "Iyashikei"
+    		},{
+    			"title": "Love Polygon",
+    			"label": "Love Polygon"
+    		},{
+    			"title": "Magical Sex Shift",
+    			"label": "Magical Sex Shift"
+    		},{
+    			"title": "Mahou Shoujo",
+    			"label": "Mahou Shoujo"
+    		},{
+    			"title": "Martial Arts",
+    			"label": "Martial Arts"
+    		},{
+    			"title": "Mecha",
+    			"label": "Mecha"
+    		},{
+    			"title": "Medical",
+    			"label": "Medical"
+    		},{
+    			"title": "Military",
+    			"label": "Military"
+    		},{
+    			"title": "Music",
+    			"label": "Music"
+    		},{
+    			"title": "Mythology",
+    			"label": "Mythology"
+    		},{
+    			"title": "Organized Crime",
+    			"label": "Organized Crime"
+    		},{
+    			"title": "Otaku Culture",
+    			"label": "Otaku Culture"
+    		},{
+    			"title": "Parody",
+    			"label": "Parody"
+    		},{
+    			"title": "Performing Arts",
+    			"label": "Performing Arts"
+    		},{
+    			"title": "Pets",
+    			"label": "Pets"
+    		},{
+    			"title": "Pyschological",
+    			"label": "Pyschological"
+    		},{
+    			"title": "Racing",
+    			"label": "Racing"
+    		},{
+    			"title": "Reincarnation",
+    			"label": "Reincarnation"
+    		},{
+    			"title": "Reverse Harem",
+    			"label": "Reverse Harem"
+    		},{
+    			"title": "Romantic Subtext",
+    			"label": "Romantic Subtext"
+    		},{
+    			"title": "Samurai",
+    			"label": "Samurai"
+    		},{
+    			"title": "School",
+    			"label": "School"
+    		},{
+    			"title": "Showbiz",
+    			"label": "Showbiz"
+    		},{
+    			"title": "Space",
+    			"label": "Space"
+    		},{
+    			"title": "Strategy Game",
+    			"label": "Strategy Game"
+    		},{
+    			"title": "Super Power",
+    			"label": "Super Power"
+    		},{
+    			"title": "Survival",
+    			"label": "Survival"
+    		},{
+    			"title": "Team Sports",
+    			"label": "Team Sports"
+    		},{
+    			"title": "Time Travel",
+    			"label": "Time Travel"
+    		},{
+    			"title": "Vampire",
+    			"label": "Vampire"
+    		},{
+    			"title": "Video Game",
+    			"label": "Video Game"
+    		},{
+    			"title": "Visual Arts",
+    			"label": "Visual Arts"
+    		},{
+    			"title": "Workplace",
+    			"label": "Workplace"
+    		}
+    	]
+    },{
+    	"type": "Demographics",
+    	"list": [
+    		{
+    			"title": "Josei",
+    			"label": "Josei"
+    		},{
+    			"title": "Kids",
+    			"label": "Kids"
+    		},{
+    			"title": "Seinen",
+    			"label": "Seinen"
+    		},{
+    			"title": "Shoujo",
+    			"label": "Shoujo"
+    		},{
+    			"title": "Shounen",
+    			"label": "Shounen"
+    		}
+    	]
+    }
+  ],
+  "Drama": [],
+  "Music": []
+};
