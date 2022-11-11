d104fdb19b07d44d5429fff6add0ed0f6c779f92
Jonariar Deva Wardhani <47747520+devaward@users.noreply.github.com>
2022-11-12 00:14:15 +0700

    Update genrelist.js

--- GuraNime/v4.1/assets/js/genrelist.js
+++ GuraNime/v4.1/assets/js/genrelist.js
@@ -1,252 +1,65 @@
-let genrelist = {
-  "anime" : [
-    {
-      "type": "Genres",
-      "list": [
-      	{
-      		"title": "Action",
-      		"label" : "Action"
-      	},{
-      		"title": "Adventure",
-      		"label" : "Adventure"
-      	},{
-      		"title": "Avant Garde",
-      		"label" : "Avant Garde"
-      	},{
-      		"title": "Award Winning",
-      		"label" : "Award Winning"
-      	},{
-      		"title": "Boys Love",
-      		"label" : "Boys Love"
-      	},{
-      		"title": "Comedy",
-      		"label" : "Comedy"
-      	},{
-      		"title": "Drama",
-      		"label" : "Drama"
-      	},{
-      		"title": "Fantasy",
-      		"label" : "Fantasy"
-      	},{
-      		"title": "Girls Love",
-      		"label" : "Girls Love"
-      	},{
-      		"title": "Gourmet",
-      		"label" : "Gourmet"
-      	},{
-      		"title": "Horror",
-      		"label" : "Horror"
-      	},{
-      		"title": "Mystery",
-      		"label" : "Mystery"
-      	},{
-      		"title": "Romance",
-      		"label" : "Romance"
-      	},{
-      		"title": "Sci-Fi",
-      		"label" : "Sci-Fi"
-      	},{
-      		"title": "Slice of Life",
-      		"label" : "Slice of Life"
-      	},{
-      		"title": "Sports",
-      		"label" : "Sports"
-      	},{
-      		"title": "Supernatural",
-      		"label" : "Supernatural"
-      	},{
-      		"title": "Suspense",
-      		"label" : "Suspense"
-      	}
-      ]
-    },{
-    	"type": "Explicit Genres",
-    	"list": [
-    		{
-    			"title": "Ecchi",
-    			"label": "Ecchi"
-    		},{
-    			"title": "Erotica",
-    			"label": "Erotica"
-    		},{
-    			"title": "Hentai",
-    			"label": "Hentai"
-    		}
-    	]
-    },{
-    	"type": "Themes",
-    	"list": [
-    		{
-    			"title": "Adult Cast",
-    			"label": "Adult Cast"
-    		},{
-    			"title": "Anthromorphic",
-    			"label": "Anthromorphic"
-    		},{
-    			"title": "CGDCT",
-    			"label": "CGDCT"
-    		},{
-    			"title": "Childcare",
-    			"label": "Childcare"
-    		},{
-    			"title": "Combat Sports",
-    			"label": "Combat Sports"
-    		},{
-    			"title": "Crossdressing",
-    			"label": "Crossdressing"
-    		},{
-    			"title": "Delinquents",
-    			"label": "Delinquents"
-    		},{
-    			"title": "Detective",
-    			"label": "Detective"
-    		},{
-    			"title": "Educational",
-    			"label": "Educational"
-    		},{
-    			"title": "Gag Humor",
-    			"label": "Gag Humor"
-    		},{
-    			"title": "Gore",
-    			"label": "Gore"
-    		},{
-    			"title": "Harem",
-    			"label": "Harem"
-    		},{
-    			"title": "Historical",
-    			"label": "Historical"
-    		},{
-    			"title": "Idols (Female)",
-    			"label": "Idols (Female)"
-    		},{
-    			"title": "Idols (Male)",
-    			"label": "Idols (Male)"
-    		},{
-    			"title": "Isekai",
-    			"label": "Isekai"
-    		},{
-    			"title": "Iyashikei",
-    			"label": "Iyashikei"
-    		},{
-    			"title": "Love Polygon",
-    			"label": "Love Polygon"
-    		},{
-    			"title": "Magical Sex Shift",
-    			"label": "Magical Sex Shift"
-    		},{
-    			"title": "Mahou Shoujo",
-    			"label": "Mahou Shoujo"
-    		},{
-    			"title": "Martial Arts",
-    			"label": "Martial Arts"
-    		},{
-    			"title": "Mecha",
-    			"label": "Mecha"
-    		},{
-    			"title": "Medical",
-    			"label": "Medical"
-    		},{
-    			"title": "Military",
-    			"label": "Military"
-    		},{
-    			"title": "Music",
-    			"label": "Music"
-    		},{
-    			"title": "Mythology",
-    			"label": "Mythology"
-    		},{
-    			"title": "Organized Crime",
-    			"label": "Organized Crime"
-    		},{
-    			"title": "Otaku Culture",
-    			"label": "Otaku Culture"
-    		},{
-    			"title": "Parody",
-    			"label": "Parody"
-    		},{
-    			"title": "Performing Arts",
-    			"label": "Performing Arts"
-    		},{
-    			"title": "Pets",
-    			"label": "Pets"
-    		},{
-    			"title": "Pyschological",
-    			"label": "Pyschological"
-    		},{
-    			"title": "Racing",
-    			"label": "Racing"
-    		},{
-    			"title": "Reincarnation",
-    			"label": "Reincarnation"
-    		},{
-    			"title": "Reverse Harem",
-    			"label": "Reverse Harem"
-    		},{
-    			"title": "Romantic Subtext",
-    			"label": "Romantic Subtext"
-    		},{
-    			"title": "Samurai",
-    			"label": "Samurai"
-    		},{
-    			"title": "School",
-    			"label": "School"
-    		},{
-    			"title": "Showbiz",
-    			"label": "Showbiz"
-    		},{
-    			"title": "Space",
-    			"label": "Space"
-    		},{
-    			"title": "Strategy Game",
-    			"label": "Strategy Game"
-    		},{
-    			"title": "Super Power",
-    			"label": "Super Power"
-    		},{
-    			"title": "Survival",
-    			"label": "Survival"
-    		},{
-    			"title": "Team Sports",
-    			"label": "Team Sports"
-    		},{
-    			"title": "Time Travel",
-    			"label": "Time Travel"
-    		},{
-    			"title": "Vampire",
-    			"label": "Vampire"
-    		},{
-    			"title": "Video Game",
-    			"label": "Video Game"
-    		},{
-    			"title": "Visual Arts",
-    			"label": "Visual Arts"
-    		},{
-    			"title": "Workplace",
-    			"label": "Workplace"
-    		}
-    	]
-    },{
-    	"type": "Demographics",
-    	"list": [
-    		{
-    			"title": "Josei",
-    			"label": "Josei"
-    		},{
-    			"title": "Kids",
-    			"label": "Kids"
-    		},{
-    			"title": "Seinen",
-    			"label": "Seinen"
-    		},{
-    			"title": "Shoujo",
-    			"label": "Shoujo"
-    		},{
-    			"title": "Shounen",
-    			"label": "Shounen"
-    		}
-    	]
+window.addEventListener('load', async () => {
+    try {
+      /* [Ambil semua genre di blogger] */
+      let url_api = 'https://blogger.adb.my.id/';
+      let get = await fetch(url_api + 'alat.php?target=' + window.location.hostname + '&action=all_genres');
+      let json = await get.json();
+
+      /*[Data Genre Ambil juga]*/
+      let genrelist = 'https://themes.adb.my.id/GuraNime/v4.1/assets/json/genrelist.json';
+      let get_2 = await fetch(genrelist);
+      let json_2 = await get_2.json();
+
+      /* [dom] */
+      let genredom = document.querySelector('#jona_genrelist');
+
+      let loop = (data, title) => {
+        if(data && data.length > 0) {
+          let genres = document.createElement('div');
+          let title_g = document.createElement('h3');
+          genres.classList.add('genres');
+          title_g.innerText = title;
+          genredom.appendChild(genres).appendChild(title_g);
+          for(let {type, list} of data) {
+            let type_g = document.createElement('div');
+            let type_title_g = document.createElement('h3');
+            let lists_g = document.createElement('div');
+            type_title_g.innerText = type;
+            type_g.classList.add('type_g');
+            type_g.setAttribute('type',type);
+            type_g.appendChild(type_title_g);
+            type_g.appendChild(lists_g);
+            lists_g.classList.add('lists_g');
+            genres.appendChild(type_g);
+
+            /* [check] */
+            let exists = false;
+            let count = 0;
+            for(genre of json) {
+              count++;
+              list.map(e => {
+                if(e.label == genre) {
+                  exists = true;
+                  let list_g = document.createElement('span');
+                  lists_g.appendChild(list_g);
+                  list_g.innerText = e.title;
+                  list_g.onclick = () => {
+                    location.replace('/search/label/'+e.label);
+                  }
+                }
+              });
+
+              /* [check] */
+              console.log(count);
+              count == list.length ? (exists==false?document.querySelector('.type_g[type="'+type+'"]').remove():false):false; 
+            }
+          }
+        }
+      }
+      loop(json_2.anime, 'Anime');
+      loop(json_2.music, 'Music');
+      loop(json_2.drama, 'Drama');
+    } catch(e) {
+      console.log(e);
     }
-  ],
-  "Drama": [],
-  "Music": []
-};
+  });
